f53dd1d0f8785e72f26caf55d3b96190945f29bf
Phuoc Loc Nguyen Tran <phuocloc742@gmail.com>
2024-10-31 16:33:02 +0700

    second commit

---
+++ 3D_Video_Game/Obstacle Dodge/Assets/Scripts/Mover.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField]float xValue = 0;
+    [SerializeField]float yValue = 0.01f;
+    [SerializeField]float zValue = 0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(xValue, yValue, zValue);
+    }
+}
