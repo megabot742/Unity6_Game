cdcb60b8ee794f270009fe1b0c9b7ec96cdc65aa
Phuoc Loc Nguyen Tran <phuocloc742@gmail.com>
2024-11-04 19:48:10 +0700

    fifth commit

--- 3D_Video_Game/Obstacle Dodge/Assets/Scripts/Mover.cs
+++ 3D_Video_Game/Obstacle Dodge/Assets/Scripts/Mover.cs
@@ -6,11 +6,21 @@ public class Mover : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        PrintInstructuon();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        MovePlayer();
+    }
+    void PrintInstructuon()
+    {
+        Debug.Log("Welcome to the game!");
+        Debug.Log("Move using arrow keys or WASD");
+        Debug.Log("Try to avoid the obstacles");
+    }
+    void MovePlayer()
     {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float yValue = 0f;
