195090bceeaacedde661043d7ecb32e2cde3c38b
Phuoc Loc Nguyen Tran <phuocloc742@gmail.com>
2024-11-10 17:18:31 +0700

    eleventh commit

--- 3D_Video_Game/Obstacle Dodge/Assets/Scripts/FlyAtPlayer.cs
+++ 3D_Video_Game/Obstacle Dodge/Assets/Scripts/FlyAtPlayer.cs
@@ -6,10 +6,13 @@ public class FlyAtPlayer : MonoBehaviour
     [SerializeField] float moveSpeed = 0f;
     Vector3 playerPostion;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake() 
+    {
+        gameObject.SetActive(false); 
+    }
     void Start()
     {
         playerPostion = player.transform.position;
-        
     }
     // Update is called once per frame
     void Update()
