5daa92caad26b936cabaea96a502f30b2869254a
Phuoc Loc Nguyen Tran <phuocloc742@gmail.com>
2024-11-12 17:59:00 +0700

    fourteenth commit

---
+++ 3D_Video_Game/Rocket Boost/Assets/GameDevTV Assets/Scripts/Movement.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    void Start()
+    {
+        //Variables
+        //Method
+    }
+    void Update()
+    {
+        
+    }
+}
