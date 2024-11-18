fdde2a194ecbc97103f455a76f1abca2e42c5fc9
Phuoc Loc Nguyen Tran <phuocloc742@gmail.com>
2024-11-18 19:29:50 +0700

    twentieth commit

--- 3D_Video_Game/Rocket Boost/Assets/Scripts/CollisionHandler.cs
+++ 3D_Video_Game/Rocket Boost/Assets/Scripts/CollisionHandler.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
@@ -6,18 +7,35 @@ public class CollisionHandler : MonoBehaviour
     {
         switch (other.gameObject.tag)
         {
-            case "Finish":
-                Debug.Log("Landing");
-                break;
             case "Fuel":
                 Debug.Log("Get Fule");
                 break;
             case "Friendly":
                 Debug.Log("Ready to fly");
                 break;
+            case "Finish":
+                Debug.Log("Landing");
+                LoadNextLevel();
+                break;
             default:
-                Debug.Log("You crash");
+                ReloadLevel();
                 break;
         }
     }
+    void LoadNextLevel()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        int nextScene = currentScene + 1;
+        if(nextScene == SceneManager.sceneCountInBuildSettings)
+        {
+            nextScene = 0;
+        }
+        SceneManager.LoadScene(nextScene);
+    }
+    void ReloadLevel()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex; // get current scene when playing
+        SceneManager.LoadScene(currentScene); // reload current scene
+    }
+    
 }
