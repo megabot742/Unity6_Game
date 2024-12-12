97feab9960e23e8c024fa97ba13da84a619b6e5e
Phuoc Loc Nguyen Tran <phuocloc742@gmail.com>
2024-12-12 17:41:55 +0700

    thirty first commit

--- 3D_Video_Game/Galaxy Strike/Assets/Scripts/PlayerMovoments.cs
+++ 3D_Video_Game/Galaxy Strike/Assets/Scripts/PlayerMovoments.cs
@@ -3,19 +3,43 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovoments : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Moveoment")]
+    [SerializeField] float controlSpeed = 10f;
+    [SerializeField] float xClampRange = 35f;
+    [SerializeField] float yClampRange = 35f;
 
-    // Update is called once per frame
+    [Header("Rotation")]
+    [SerializeField] float controlRollFactor = 20f;
+    [SerializeField] float controlPitchFactor = 20f;
+    [SerializeField] float rotationSpeed = 20f;
+
+    Vector2 movement;
     void Update()
     {
-        
+        ProcessTranslation();
+        ProcessRotation();
     }
     public void OnMove(InputValue value)
     {
-        Debug.Log(value.Get<Vector2>());
+       movement = value.Get<Vector2>();
+    }
+    void ProcessTranslation()
+    {
+        float xOffset = movement.x * controlSpeed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xClampRange, xClampRange);
+
+
+        float yOffset = movement.y * controlSpeed * Time.deltaTime;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yClampRange, yClampRange);
+        ; transform.localPosition = new Vector3(clampedXPos, clampedYPos , 0f);
+    }
+    void ProcessRotation()
+    {
+        float roll = -controlRollFactor*movement.x;
+        float pitch = -controlPitchFactor*movement.y;
+        Quaternion targetRotation = Quaternion.Euler(pitch, 0f, roll); //The flight direction must be opposite movement
+        transform.localRotation = Quaternion.Lerp(transform.localRotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
 }
