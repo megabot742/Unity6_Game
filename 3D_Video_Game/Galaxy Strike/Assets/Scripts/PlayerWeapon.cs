30c9dc6ca03ceaeb3968a3e1044af6350baae558
Phuoc Loc Nguyen Tran <phuocloc742@gmail.com>
2024-12-14 22:48:24 +0700

    thirty third commit

--- 3D_Video_Game/Galaxy Strike/Assets/Scripts/PlayerWeapon.cs
+++ 3D_Video_Game/Galaxy Strike/Assets/Scripts/PlayerWeapon.cs
@@ -1,13 +1,24 @@
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerWeapon : MonoBehaviour
 {
-    [SerializeField] GameObject laser;
+    [SerializeField] GameObject[] lasers;
+    [SerializeField] RectTransform crossHair;
+    [SerializeField] Transform targetPoint;
+    [SerializeField] float targetDistance = 100f;
     bool isFiring = false;
+    void Start()
+    {
+        Cursor.visible = false;
+    }
     void Update()
     {
         ProcessFiring();
+        MoveCrosshair();
+        MoveTargetPoint();
+        AimLaser();
     }
     void OnFire(InputValue value)
     {
@@ -15,7 +26,28 @@ public class PlayerWeapon : MonoBehaviour
     }
     void ProcessFiring()
     {
-        var emmissionModule = laser.GetComponent<ParticleSystem>().emission;
-        emmissionModule.enabled = isFiring;
+        foreach(GameObject item in lasers)
+        {
+            var emmissionModule = item.GetComponent<ParticleSystem>().emission;
+            emmissionModule.enabled = isFiring;
+        }
+    }
+    void MoveCrosshair()
+    {
+        crossHair.position = Input.mousePosition;
+    }
+    void MoveTargetPoint()
+    {
+        Vector3 targetPointPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, targetDistance);
+        targetPoint.position = Camera.main.ScreenToWorldPoint(targetPointPosition);
+    }
+    void AimLaser()
+    {
+        foreach(GameObject laser in lasers)
+        {
+            Vector3 fireDirection = targetPoint.position - transform.position; //ship tranform position
+            Quaternion rotationToTarget = Quaternion.LookRotation(fireDirection);
+            laser.transform.rotation = rotationToTarget;
+        }
     }
 }
