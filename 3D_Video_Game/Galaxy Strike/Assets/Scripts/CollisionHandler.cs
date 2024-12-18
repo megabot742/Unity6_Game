ea181f0d8e15ddfb9ba365364f25557def859e7a
Phuoc Loc Nguyen Tran <phuocloc742@gmail.com>
2024-12-18 18:18:54 +0700

    thirty fifth commit

--- 3D_Video_Game/Galaxy Strike/Assets/Scripts/CollisionHandler.cs
+++ 3D_Video_Game/Galaxy Strike/Assets/Scripts/CollisionHandler.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] ParticleSystem destroyVFX;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,5 +16,7 @@ public class CollisionHandler : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other) {
         Debug.Log("Hit" + other.name);
+        Instantiate(destroyVFX,transform.position, Quaternion.identity); // Quaternion.identity = default rotations
+        Destroy(gameObject);
     }
 }
