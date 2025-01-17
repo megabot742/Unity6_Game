7e1a0530c8a9864429e3ef2837e2412549185fdb
Phuoc Loc Nguyen Tran <phuocloc742@gmail.com>
2025-01-17 20:47:14 +0700

    fifth sixth commit

---
+++ 3D_Video_Game/Laser Shooter/Assets/Scripts/Weapon.cs
@@ -0,0 +1,28 @@
+using StarterAssets;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    //[SerializeField] float maxDistance = 20f;
+    // Update is called once per frame
+    [SerializeField] int hitDamage = 1;
+    StarterAssetsInputs starterAssetsInputs;
+    private void Awake() {
+        starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
+    } 
+    void Update()
+    {
+        HandleShoot();
+    }
+    void HandleShoot()
+    {
+        if(!starterAssetsInputs.shoot) {return;}
+        RaycastHit hit;
+        if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward,out hit, Mathf.Infinity))
+        {
+            EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
+            enemyHealth?.TakeDamage(hitDamage);
+            starterAssetsInputs.ShootInput(false);
+        }
+    }
+}
