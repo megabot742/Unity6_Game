0ac604b813a54e548c16f987a061dbed92fcbf69
Phuoc Loc Nguyen Tran <phuocloc742@gmail.com>
2025-01-19 23:34:58 +0700

    fifty eighth commit

--- 3D_Video_Game/Laser Shooter/Assets/Scripts/Weapon.cs
+++ 3D_Video_Game/Laser Shooter/Assets/Scripts/Weapon.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     //[SerializeField] float maxDistance = 20f;
-    // Update is called once per frame
-    [SerializeField] int hitDamage = 1;
+    
+    [SerializeField] Animator animator;
     [SerializeField] ParticleSystem muzzleFlashFX;
+    [SerializeField] int hitDamage = 1;
+    [SerializeField] GameObject hitFXPrefabs;
     StarterAssetsInputs starterAssetsInputs;
+    const string SHOOT_STRING = "Shoot";
     private void Awake() {
         starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
     } 
@@ -18,13 +21,15 @@ public class Weapon : MonoBehaviour
     void HandleShoot()
     {
         if(!starterAssetsInputs.shoot) {return;}
-        muzzleFlashFX.Play();
+        muzzleFlashFX.Play(); //VFX gun
+        animator.Play(SHOOT_STRING, 0, 0f); //Animation gun
+        starterAssetsInputs.ShootInput(false); //reload status 
         RaycastHit hit;
         if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward,out hit, Mathf.Infinity))
         {
+            Instantiate(hitFXPrefabs, hit.point, Quaternion.identity);
             EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
             enemyHealth?.TakeDamage(hitDamage);
-            starterAssetsInputs.ShootInput(false); 
         }
     }
 }
