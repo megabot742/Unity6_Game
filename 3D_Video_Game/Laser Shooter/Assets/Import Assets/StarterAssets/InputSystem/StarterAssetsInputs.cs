60e1ab1575021fd1f226c33f43387798bcbe04be
Phuoc Loc Nguyen Tran <phuocloc742@gmail.com>
2025-01-23 23:12:06 +0700

    sixty second commit

--- 3D_Video_Game/Laser Shooter/Assets/Import Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
+++ 3D_Video_Game/Laser Shooter/Assets/Import Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
@@ -13,6 +13,7 @@ namespace StarterAssets
 		public bool jump;
 		public bool sprint;
 		public bool shoot;
+		public bool zoom;
 
 		[Header("Movement Settings")]
 		public bool analogMovement;
@@ -48,6 +49,10 @@ namespace StarterAssets
 		{
 			ShootInput(value.isPressed);
 		}
+		public void OnZoom(InputValue value)
+		{
+			ZoomInput(value.isPressed);
+		}
 #endif
 
 
@@ -73,6 +78,10 @@ namespace StarterAssets
 		public void ShootInput(bool newShootState)
 		{
 			shoot = newShootState;
+		}
+		public void ZoomInput(bool newZoomState)
+		{
+			zoom = newZoomState;
 		}
 		
 		private void OnApplicationFocus(bool hasFocus)
--- 3D_Video_Game/Laser Shooter/Assets/Scripts/ActiveWeapon.cs
+++ 3D_Video_Game/Laser Shooter/Assets/Scripts/ActiveWeapon.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
 using StarterAssets;
+using Unity.VisualScripting;
+using Unity.Mathematics;
 
 public class ActiveWeapon : MonoBehaviour
 {
     [SerializeField] WeaponSO weaponSO;
     Animator animator;
     StarterAssetsInputs starterAssetsInputs;
-    Weapon weapon;
+    Weapon currentWeapon;
     const string SHOOT_STRING = "Shoot";
     float timeCoolDownShoot = 0;
     void Awake() 
@@ -16,22 +18,49 @@ public class ActiveWeapon : MonoBehaviour
     }
     void Start() 
     {
-        weapon = GetComponentInChildren<Weapon>();
+        currentWeapon = GetComponentInChildren<Weapon>();
     }
     void Update()
     {
-        timeCoolDownShoot += Time.deltaTime;
+        
         HandleShoot();
+        HandleZoom();
+    }
+    public void SwitchWeapon(WeaponSO weaponSO)
+    {
+        if(currentWeapon)
+        {
+            Destroy(currentWeapon.gameObject);
+        }
+        Weapon newWeapon = Instantiate(weaponSO.weaponPrefab, transform).GetComponent<Weapon>();
+        currentWeapon = newWeapon;
+        this.weaponSO = weaponSO;
     }
     void HandleShoot()
     {
+        timeCoolDownShoot += Time.deltaTime;
         if(!starterAssetsInputs.shoot) {return;}
         if(timeCoolDownShoot >= weaponSO.FireRate)
         {
-            weapon.Shoot(weaponSO);
+            currentWeapon.Shoot(weaponSO);
             animator.Play(SHOOT_STRING, 0, 0f); //Animation gun
             timeCoolDownShoot = 0;
         }
-        starterAssetsInputs.ShootInput(false); //reload status 
+        if(!weaponSO.IsAutomatic)
+        {
+            starterAssetsInputs.ShootInput(false); //reload status 
+        }
+    }
+    void HandleZoom()
+    {
+        if(!weaponSO.CanZoom) {return;}
+        if(starterAssetsInputs.zoom)
+        {
+            Debug.Log("Zoom in");
+        }
+        else
+        {
+            Debug.Log("Zoom out");
+        }
     }
 }
