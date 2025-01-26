68ce3363ffef02c9b090302b3428db94e836f76f
Phuoc Loc Nguyen Tran <phuocloc742@gmail.com>
2025-01-26 16:50:17 +0700

    test ignore second

--- 3D_Video_Game/My first car/Assets/TextPrint.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class TextPrint:MonoBehaviour
-{
-    private void Start() {
-        Debug.Log("Hello therer");
-    }
-}
--- 3D_Video_Game/Royal Run/Assets/Scripts/ProcessGeneration/ObstacleDestroy.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class ObstacleDestroy : MonoBehaviour
-{
-    void OnCollisionEnter(Collision other) 
-    {
-        Destroy(other.gameObject);
-    }
-}
